25c62194d2331aa496b9455932ae8d4175602c0c
anton <anton@LAPTOP-NG1L4D9H>
2023-01-18 05:25:01 -0300

    commit inicial mentoria java

---
+++ src/main/java/jdev/mentoria/lojavirtual/MentoriaJavaLlojavirtualSpringbootApplication.java
@@ -0,0 +1,13 @@
+package jdev.mentoria.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoriaJavaLlojavirtualSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoriaJavaLlojavirtualSpringbootApplication.class, args);
+	}
+
+}
